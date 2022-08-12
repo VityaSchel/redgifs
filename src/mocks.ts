15eefe2e6f57f34d359da0eff40b8004cc1f49b2
Viktor <59040542+VityaSchel@users.noreply.github.com>
2022-08-13 02:22:02 +0400

    Mocks

--- src/mocks.ts
+++ src/mocks.ts
@@ -1,12 +1,14 @@
 // THIS FILE is intended to be a bridge between mtproto wrapper and code
 
+const peer = () => ({
+  _: 'inputPeerChannel',
+  channel_id: global.api.TARGET_CHANNEL_ID,
+  access_hash: global.api.TARGET_CHANNEL_ACCESS_HASH
+})
+
 export async function sendMessage(text: string, replyMessageID?: number) {
   return await global.api.call('messages.sendMessage', {
-    peer: {
-      _: 'inputPeerChannel',
-      channel_id: global.api.TARGET_CHANNEL_ID,
-      access_hash: global.api.TARGET_CHANNEL_ACCESS_HASH
-    },
+    peer: peer(),
     ...(replyMessageID && { reply_to_msg_id: replyMessageID }),
     message: text,
     random_id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
@@ -15,16 +17,39 @@ export async function sendMessage(text: string, replyMessageID?: number) {
 
 export async function editMessageText(messageID: number, text: string) {
   return await global.api.call('messages.editMessage', {
-    peer: {
-      _: 'inputPeerChannel',
-      channel_id: global.api.TARGET_CHANNEL_ID,
-      access_hash: global.api.TARGET_CHANNEL_ACCESS_HASH
-    },
+    peer: peer(),
     id: messageID,
     message: text
   })
 }
 
-export async function sendPhoto() {
+export async function sendPhoto(photoURL: string, replyMessageID?: number) {
+  return await global.api.call('messages.sendMedia', {
+    peer: peer(),
+    ...(replyMessageID && { reply_to_msg_id: replyMessageID }),
+    media: {
+      _: 'inputMediaPhotoExternal',
+      url: photoURL
+    },
+    random_id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+  })
+}
 
+export async function sendVideo(videoURL: string, replyMessageID?: number) {
+  return await global.api.call('messages.sendMedia', {
+    peer: peer(),
+    ...(replyMessageID && { reply_to_msg_id: replyMessageID }),
+    media: {
+      _: 'inputMediaDocumentExternal',
+      url: videoURL
+    },
+    random_id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+  })
+}
+
+export async function deleteMessage(messageID: number) {
+  return await global.api.call('channels.deleteMessages', {
+    channel: peer(),
+    id: [messageID]
+  })
 }
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,87 +1,88 @@
 import './.env.js'
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 import fs from 'fs/promises'
 import $D from 'dedent'
-import { editMessageText, sendMessage } from './mocks'
+import { deleteMessage, editMessageText, sendMessage, sendPhoto, sendVideo } from './mocks'
 
 const loadingPool = {}
 
-// async function load(target, source = 'reddit') {
-//   switch(source) {
-//     case 'reddit':
-//     case 'pushshift':
-//       return await reddit(target, source)
+async function load(target, source = 'reddit') {
+  switch(source) {
+    case 'reddit':
+    case 'pushshift':
+      return await reddit(target, source)
 
-//     case 'waifupics':
-//       return await waifupics(target)
-//   }
-// }
+    case 'waifupics':
+      return await waifupics(target)
+  }
+}
 
-// async function waifupics(target) {
-//   const response = await fetch(`https://api.waifu.pics/nsfw/${target}`)
-//   const result = await response.json()
-//   const url = result.url
-//   return [url]
-// }
+async function waifupics(target): Promise<string[]> {
+  const response = await fetch(`https://api.waifu.pics/nsfw/${target}`)
+  const result = await response.json()
+  const url = result['url']
+  return [url]
+}
 
-// async function reddit(subreddit, source = 'reddit') {
-//   // if(i >= 4) throw 'Couldn\'t find any :('
+async function reddit(subreddit, source = 'reddit'): Promise<string[]> {
+  // if(i >= 4) throw 'Couldn\'t find any :('
 
-//   const yearAgo = (Date.now() - 1000 * 60 * 60 * 24 * 365)
-//   const _2013 = 1356984630000
-//   let before
-//   if(source === 'pushshift')
-//     if(subreddit === 'gore')
-//       before = Math.floor(Math.random() * (Date.now() - _2013) + _2013)
-//     else
-//       before = Math.floor(Math.random() * (Date.now() - yearAgo) + yearAgo)
+  const yearAgo = (Date.now() - 1000 * 60 * 60 * 24 * 365)
+  const _2013 = 1356984630000
+  let before
+  if(source === 'pushshift')
+    if(subreddit === 'gore')
+      before = Math.floor(Math.random() * (Date.now() - _2013) + _2013)
+    else
+      before = Math.floor(Math.random() * (Date.now() - yearAgo) + yearAgo)
 
-//   const query = source === 'reddit' ? `https://api.reddit.com/r/${subreddit}/random?limit=1` : `https://api.pushshift.io/reddit/search/submission/?q=*&before=${Math.floor(before / 1000)}&subreddit=${subreddit}&aggs=&metadata=true&advanced=false&sort=desc&domain=&sort_type=created_utc&size=5`
-//   console.log(query)
+  const query = source === 'reddit' ? `https://api.reddit.com/r/${subreddit}/random?limit=1` : `https://api.pushshift.io/reddit/search/submission/?q=*&before=${Math.floor(before / 1000)}&subreddit=${subreddit}&aggs=&metadata=true&advanced=false&sort=desc&domain=&sort_type=created_utc&size=5`
+  console.log(query)
 
-//   let controller = new AbortController()
-//   const timeoutAbort = setTimeout(() => controller.abort(), 10000)
+  const controller = new AbortController()
+  const timeoutAbort = setTimeout(() => controller.abort(), 10000)
 
-//   const response = await fetch(query, { signal: controller.signal })
-//   let result = await response.json()
-//   if (Array.isArray(result)) result = result[0]
-//   // console.log(JSON.stringify(result, null, 2))
-//   if (source === 'reddit' && !result.data) await fs.writeFile('test.json', JSON.stringify(result, null, 2))
-//   const mediaData = source === 'reddit' ? result.data.children : result.data
-//   const mediaURL = []
-//   for(let m of mediaData) {
-//     const data = source === 'reddit' ? m.data : m
-//     if (data.selftext) continue
-//     // if (m.data.url.startsWith('https://i.redd.it/')) https://i.imgur.com
-//     const url = data.url
-//     if (['.png', '.jpg', '.jpeg', '.gif', '.gifv', '.mp4'].some(extension => url.endsWith(extension)))
-//       if (url.startsWith('https://i.imgur.com/') && (url.endsWith('.gifv') || url.endsWith('.gif')))
-//         mediaURL.push(`https://i.imgur.com/${url.match(/^https:\/\/i\.imgur\.com\/(\w+)\.gifv?$/)[1]}.mp4`)
-//       else
-//         mediaURL.push(url)
-//     else {
-//       // if (url.startsWith('https://www.reddit.com/gallery/')) return await load(subreddit, i + 1)
-//       const redgifsRegex = /^https:\/\/(www\.)?redgifs\.com\/watch\/(\w+)$/
-//       if (redgifsRegex.test(url)) {
-//         const [,, redgifid] = url.match(redgifsRegex)
-//         const redgifresp = await fetch('https://api.redgifs.com/v2/gifs/' + redgifid)
-//         const redgif = await redgifresp.json()
-//         mediaURL.push(redgif.gif.urls.sd)
-//       }/* else {
-//         const media = data.media
-//         return media?.oembed?.thumbnail_url
-//       }*/ else {
-//         // return null//await load(subreddit, i + 1)
-//         continue
-//       }
-//     }
+  // @ts-expect-error ...
+  const response = await fetch(query, { signal: controller.signal })
+  let result = await response.json()
+  if (Array.isArray(result)) result = result[0]
+  // console.log(JSON.stringify(result, null, 2))
+  if (source === 'reddit' && !result['data']) await fs.writeFile('test.json', JSON.stringify(result, null, 2))
+  const mediaData = source === 'reddit' ? result['data'].children : result['data']
+  const mediaURL = []
+  for(const m of mediaData) {
+    const data = source === 'reddit' ? m.data : m
+    if (data.selftext) continue
+    // if (m.data.url.startsWith('https://i.redd.it/')) https://i.imgur.com
+    const url = data.url
+    if (['.png', '.jpg', '.jpeg', '.gif', '.gifv', '.mp4'].some(extension => url.endsWith(extension)))
+      if (url.startsWith('https://i.imgur.com/') && (url.endsWith('.gifv') || url.endsWith('.gif')))
+        mediaURL.push(`https://i.imgur.com/${url.match(/^https:\/\/i\.imgur\.com\/(\w+)\.gifv?$/)[1]}.mp4`)
+      else
+        mediaURL.push(url)
+    else {
+      // if (url.startsWith('https://www.reddit.com/gallery/')) return await load(subreddit, i + 1)
+      const redgifsRegex = /^https:\/\/(www\.)?redgifs\.com\/watch\/(\w+)$/
+      if (redgifsRegex.test(url)) {
+        const [,, redgifid] = url.match(redgifsRegex)
+        const redgifresp = await fetch('https://api.redgifs.com/v2/gifs/' + redgifid)
+        const redgif = await redgifresp.json()
+        mediaURL.push(redgif['gif'].urls.sd)
+      }/* else {
+        const media = data.media
+        return media?.oembed?.thumbnail_url
+      }*/ else {
+        // return null//await load(subreddit, i + 1)
+        continue
+      }
+    }
 
-//     if(source !== 'reddit') break
-//   }
+    if(source !== 'reddit') break
+  }
 
-//   clearTimeout(timeoutAbort)
-//   return mediaURL
-// }
+  clearTimeout(timeoutAbort)
+  return mediaURL
+}
 
 async function stat(key) {
   const file = await fs.readFile('stats.json', 'utf-8')
@@ -89,7 +90,7 @@ async function stat(key) {
   try {
     stats = JSON.parse(file) ?? {}
   } catch(e) {
-    console.error('Oh no! Error while reading stas', file)
+    console.error('Oh no! Error while reading stats', file)
   }
   const current = Number(stats[key])
   stats[key] = Number.isInteger(current) ? current + 1 : 1
@@ -216,152 +217,150 @@ export async function newMessage(message) {
 
   const error = () => indicatorID && editMessageText(indicatorID, 'Ошибка :(')
 
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  Увerror()
+  loadingPool[issuerID] = true
+  let medias: string[] = []
+  try {
+    switch(text) {
+      case '/trap':
+        await stat('trap')
+        medias = await load('tgirls')
+        break
 
-  // loadingPool[e.from.id] = true
-  // try {
-  //   switch(text) {
-  //     case '/trap':
-  //       await stat('trap')
-  //       medias = await load('tgirls')
-  //       break
+      case '/_trap':
+      // case '/trap':
+        await stat('trap')
+        medias = await load('tgirls', 'pushshift')
+        break
 
-  //     case '/_trap':
-  //     // case '/trap':
-  //       await stat('trap')
-  //       medias = await load('tgirls', 'pushshift')
-  //       break
+      case '/govno':
+      case '/gavno':
+        await stat('govno')
+        medias = await load('poop')
+        break
 
-  //     case '/govno':
-  //     case '/gavno':
-  //       await stat('govno')
-  //       medias = await load('poop')
-  //       break
+      case '/_govno':
+      // case '/govno':
+      // case '/gavno':
+        await stat('govno')
+        medias = await load('poop', 'pushshift')
+        break
 
-  //     case '/_govno':
-  //     // case '/govno':
-  //     // case '/gavno':
-  //       await stat('govno')
-  //       medias = await load('poop', 'pushshift')
-  //       break
+      case '/penis':
+      case '/hui':
+      case '/chlen':
+      case '/pisun':
+        await stat('penis')
+        medias = await load('sounding', 'pushshift')
+        break
 
-  //     case '/penis':
-  //     case '/hui':
-  //     case '/chlen':
-  //     case '/pisun':
-  //       await stat('penis')
-  //       medias = await load('sounding', 'pushshift')
-  //       break
+      case '/pidor':
+      case '/pidar':
+      case '/pidoras':
+      case '/pidaras':
+      case '/gay':
+        await stat('gay')
+        medias = await load('gaybrosgonewild', 'pushshift')
+        break
 
-  //     case '/pidor':
-  //     case '/pidar':
-  //     case '/pidoras':
-  //     case '/pidaras':
-  //     case '/gay':
-  //       await stat('gay')
-  //       medias = await load('gaybrosgonewild', 'pushshift')
-  //       break
+      case '/bdsm':
+        await stat('bdsm')
+        medias = await load('BDSM')
+        break
 
-  //     case '/bdsm':
-  //       await stat('bdsm')
-  //       medias = await load('BDSM')
-  //       break
+      case '/_bdsm':
+      // case '/bdsm':
+        await stat('bdsm')
+        medias = await load('BDSM', 'pushshift')
+        break
 
-  //     case '/_bdsm':
-  //     // case '/bdsm':
-  //       await stat('bdsm')
-  //       medias = await load('BDSM', 'pushshift')
-  //       break
+      case '/anime':
+      case '/hentai':
+        await stat('anime')
+        medias = await load('neko', 'waifupics')
+        break
 
-  //     case '/anime':
-  //     case '/hentai':
-  //       await stat('anime')
-  //       medias = await load('neko', 'waifupics')
-  //       break
+      case '/furry':
+        await stat('furry')
+        medias = await load('yiff', 'pushshift')
+        break
 
-  //     case '/furry':
-  //       await stat('furry')
-  //       medias = await load('yiff', 'pushshift')
-  //       break
+      case '/lesb':
+      case '/lesbi':
+        await stat('lesbi')
+        medias = await load('lesbians', 'pushshift')
+        break
 
-  //     case '/lesb':
-  //     case '/lesbi':
-  //       await stat('lesbi')
-  //       medias = await load('lesbians', 'pushshift')
-  //       break
+      case '/yaoi':
+      case '/яой':
+        await stat('yaoi')
+        medias = await load('yaoi', 'pushshift')
+        break
 
-  //     case '/yaoi':
-  //     case '/яой':
-  //       await stat('yaoi')
-  //       medias = await load('yaoi', 'pushshift')
-  //       break
+      case '/jucroq':
+        await stat('jucroq')
+        medias = await load('monkeys', 'pushshift')
+        break
 
-  //     case '/jucroq':
-  //       await stat('jucroq')
-  //       medias = await load('monkeys', 'pushshift')
-  //       break
+      case '/gore':
+      case '/cp':
+      case '/zoo':
+        await stat('cp_zoo_gore')
+        loadingPool[issuerID] = false
+        await sendPhoto('AgACAgIAAxkBAAIBaWLyQThnCfAr1IyAX6pB7eII25QBAAJIvjEbuvGZS1B96ifqkC_1AQADAgADeAADKQQ', message.id)
+        return
 
-  //     case '/gore':
-  //     case '/cp':
-  //     case '/zoo':
-  //       await stat('cp_zoo_gore')
-  //       loadingPool[e.from.id] = false
-  //       await bot.sendPhoto(e.chat.id, 'AgACAgIAAxkBAAIBaWLyQThnCfAr1IyAX6pB7eII25QBAAJIvjEbuvGZS1B96ifqkC_1AQADAgADeAADKQQ', { reply_to_message_id: e.message_id })
-  //       return
+      case '/milf':
+        await stat('milf')
+        medias = await load('milf', 'pushshift')
+        break
 
-  //     case '/milf':
-  //       await stat('milf')
-  //       medias = await load('milf', 'pushshift')
-  //       break
+      case '/asians':
+        await stat('asians')
+        medias = await load('asiansgonewild', 'pushshift')
+        break
 
-  //     case '/asians':
-  //       await stat('asians')
-  //       medias = await load('asiansgonewild', 'pushshift')
-  //       break
+      case '/bigboobs':
+        await stat('bigboobs')
+        medias = await load('bigboobsgw', 'pushshift')
+        break
 
-  //     case '/bigboobs':
-  //       await stat('bigboobs')
-  //       medias = await load('bigboobsgw', 'pushshift')
-  //       break
+      case '/swingers':
+        await stat('swingers')
+        medias = await load('swingersgw', 'pushshift')
+        break
 
-  //     case '/swingers':
-  //       await stat('swingers')
-  //       medias = await load('swingersgw', 'pushshift')
-  //       break
+      case '/latex':
+        await stat('latex')
+        medias = await load('yogapants', 'pushshift')
+        break
 
-  //     case '/latex':
-  //       await stat('latex')
-  //       medias = await load('yogapants', 'pushshift')
-  //       break
+      case '/feet':
+        await stat('feet')
+        medias = await load('feet', 'pushshift')
+        break
 
-  //     case '/feet':
-  //       await stat('feet')
-  //       medias = await load('feet', 'pushshift')
-  //       break
+      case '/selffuck':
+        await stat('selffuck')
+        medias = await load('selffuck', 'pushshift')
+        break
 
-  //     case '/selffuck':
-  //       await stat('selffuck')
-  //       medias = await load('selffuck', 'pushshift')
-  //       break
-
-  //   }
-  //   for (let media of medias.slice(0, 1)) {
-  //     if (['.png', '.jpg', '.jpeg'].some(extension => media.endsWith(extension))) {
-  //       await bot.sendPhoto(e.chat.id, media, { reply_to_message_id: e.message_id })
-  //     } else if (['.gif', '.gifv', '.mp4'].some(extension => media.endsWith(extension))) {
-  //       await bot.sendVideo(e.chat.id, media, { reply_to_message_id: e.message_id })
-  //     } else {
-  //       console.log('Unknown', media)
-  //       error()
-  //       return
-  //     }
-  //   }
-  //   loadingPool[e.from.id] = false
-  //   await bot.deleteMessage(e.chat.id, indicator.message_id)
-  // } catch (err) {
-  //   loadingPool[e.from.id] = false
-  //   console.error(err)
-  //   error()
-  // }
+    }
+    for (const media of medias.slice(0, 1)) {
+      if (['.png', '.jpg', '.jpeg'].some(extension => media.endsWith(extension))) {
+        await sendPhoto(media, message.id)
+      } else if (['.gif', '.gifv', '.mp4'].some(extension => media.endsWith(extension))) {
+        await sendVideo(media, message.id)
+      } else {
+        console.log('Unknown', media)
+        error()
+        return
+      }
+    }
+    loadingPool[issuerID] = false
+    await deleteMessage(indicatorID)
+  } catch (err) {
+    loadingPool[issuerID] = false
+    console.error(err)
+    error()
+  }
 }
