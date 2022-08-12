0ce1e7578f140f3b8ecfeb130072ab45efc50ef7
Viktor <59040542+VityaSchel@users.noreply.github.com>
2022-08-13 00:15:16 +0400

    Moving to MTproto

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config'
+import MTProto from '@mtproto/core'
+import authorize from './mtproto/auth'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) + '/'
+
+
+const bot = new MTProto({
+  api_id: Number(process.env.APP_ID),
+  api_hash: process.env.APP_HASH,
+
+  storageOptions: { path: __dirname + '../tempdata.json' }
+})
+global.api = bot
+const user = await authorize()
+console.log(`Пользователь ${user.users[0].first_name} авторизирован, бот начинает работу`)
+
+const timeouts: { [key: string]: number } = {
+  respondedToUser: 0
+}
+
+global.api.updates.on('updateShortMessage', async updateInfo => {
+  if(updateInfo.out === false) {
+    checkLatestDialogs(updateInfo)
+  }
+})
+global.api.updates.on('updates', ({ updates }) => /*updates
+  .filter(({ _ }) => _ === 'updateNewMessage')
+  .filter(({ message }) => !message.out)
+  .filter(({ message }) => message.peer_id._ === 'peerUser')
+  .length > 0 &&*/ checkLatestDialogs(updates)
+)
+
+const ЗАПРЕЩЕННЫЙ_ПЯТОЧЕК_CHANNEL_ID = '1507213786'
+
+function checkLatestDialogs(events) {
+  if (!Array.isArray(events)) events = [events]
+  for (const event of events) {
+    if (!event) continue
+    if (event._ === 'updateNewChannelMessage') {
+      const message = event.message
+      if (!message || message._ !== 'message') continue
+      if (message.peer_id?.channel_id !== ЗАПРЕЩЕННЫЙ_ПЯТОЧЕК_CHANNEL_ID) continue
+      if (message.fwd_from) continue // includes hidden users
+      const text = message.text
+      if(!text) continue
+
+      console.log(JSON.stringify(event, null, 2))
+    }
+  }
+}
+
+// Use https://mtproto-core.js.org/docs/ to write methods here
